9e1b777beb21236874c5adf1d9dabd8e815d1845
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 17:09:21 +0700

    Get multiple fields values via HMGET

--- playground/index.js
+++ playground/index.js
@@ -1,17 +1,16 @@
 /*
-  Using HSET, HGET and HGETALL for hashes
-  - pic
+  Get multiple fields values via HMGET
+  - in case we want to select specific fields, we don't can't use HGETALL
+    > HMGET <field> <field>
 
-  - hset user fname "John" lname "Doe"
-  - hget user fname
-  - hget user lname
-  - hget user lname1
-  - hgetall user
-  - hset user age 40
-  - hget user age
-  - hgetall user
-  - hset user age 35
-  - hget user age
-  - hgetall user
+  - pic
   
+  - hgetall user
+  - rename user user:101
+  - hgetall user:101
+  - hmget user:101 fname lname
+  - hmget user:101 fname lname lname1
+  - hmget user:101 age fname lname
+
+
 */
