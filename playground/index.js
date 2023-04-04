68212a2da123249d55f604d790d3928448bf42b9
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 14:07:03 +0700

    Find matching elements via LPOS

--- playground/index.js
+++ playground/index.js
@@ -1,10 +1,30 @@
 /*
-  Find length of a list via LLEN
+  Find matching elements via LPOS
   - pic
 
-  - rpush app:config:lst_supported_lang "English" "Japanese"
-  - rpush app:config:lst_supported_lang "English" "Japanese" "Arabic" "Chinese"
-  - lrange app:config:lst_supported_lang 0 -1
-  - llen app:config:lst_supported_lang
+  - rpush mykey a b a a c d e f a
+  - lrange mykey 0 -1
+  - lpos mykey "a"
+  - lpos mykey "a" rank 0
+  - lpos mykey "a" rank 1
+  - lpos mykey "a" rank 2
+  - lpos mykey "a" rank 3
+  - lpos mykey "a" rank -1
+  - lpos mykey "a" rank -2
+  - lpos mykey "a" count
+  - lpos mykey "a" count 0
+  - lpos mykey "a" count 1
+  - lpos mykey "a" count 2
+  - lpos mykey "a" count 3
+  - lpos mykey "a" count 4
+  - lpos mykey "a" rank 1 count 0
+  - lpos mykey "a" rank -1 count 0
+  - lpos mykey "a" rank -1 count 1
+  - lpos mykey "a" rank -1 count 2
+  - lpos mykey "a" rank -1 count 2 maxlen 1
+  - lpos mykey "a" rank 1 count 2 maxlen 1
+  - lpos mykey "a" rank 1 count 2 maxlen 10
+  - lpos mykey "a" rank 1 count 2 maxlen 100
+  - lpos mykey "a" rank 1 count 0 maxlen 5000
 
 */
