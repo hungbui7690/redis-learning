b2aa6bb5d6ce2ec9500dc553db71f23cf48a274c
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 10:28:01 +0700

    Set Key and expiration using SETEX and PSETEX

--- playground/index.js
+++ playground/index.js
@@ -1,5 +1,16 @@
 /*
-  Replace string value using SETRANGE
+  Set Key and expiration using SETEX and PSETEX
   - pic
+
+  - set num 1
+  - get num
+  - expire num 10
+  - ttl num
+  - setex num 120 1
+  - ttl num
+  - setex app:config:timeout 100 1
+  - ttl app:config:timeout
+  - psetnx num 10000 1
+  - ttl num
 
 */
