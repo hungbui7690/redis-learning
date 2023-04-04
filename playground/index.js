5433b878a7266c1660ec94f5f2aea6ca72dbd2be
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 16:17:36 +0700

    Using HSET, HGET and HGETALL for hashes

--- playground/index.js
+++ playground/index.js
@@ -1,6 +1,17 @@
 /*
-  Introduction to Hashes
+  Using HSET, HGET and HGETALL for hashes
   - pic
 
+  - hset user fname "John" lname "Doe"
+  - hget user fname
+  - hget user lname
+  - hget user lname1
+  - hgetall user
+  - hset user age 40
+  - hget user age
+  - hgetall user
+  - hset user age 35
+  - hget user age
+  - hgetall user
   
 */
