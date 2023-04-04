80bf7e3126cbb091c15dcf093cf17c87b8a0d7a6
hungbui7690 <hungbui7690@gmail.com>
2023-04-05 04:23:50 +0700

    Using SETS for unique data

--- playground/index.js
+++ playground/index.js
@@ -1,20 +1,28 @@
 /*
-  Add, Get Members in Set
-  - pics
-    - add set > use "sadd"
-    - to get the elements > "smembers"
+  Using SETS for unique data
+  - pic
 
-
-  - sadd cars toyota maserati ford
-  - sadd cars mazda
+  - scard cars
   - smembers cars
-  - sadd names Rob Robby Rim John
-  - smembers names
-  - sadd numbers 1 2 3
+  - scard numbers
   - smembers numbers
-  - sadd numbers 6
-  - smembers numbers
-  - sadd cars ford
-  - smembers cars
+  - scard a
+  - sadd unique_ips 1.1.1.5
+  - sadd unique_ips 1.1.1.2
+  - sadd unique_ips 1.1.1.3
+  - sadd unique_ips 1.1.1.4
+  - sadd unique_ips 1.1.1.5
+  - smembers unique_ips
+  - sadd unique_ips 1.1.1.0
+  - smembers unique_ips
+  - sadd amazon:unique_categories electronics computers
+  - smembers amazon:unique_categories
+  - sadd amazon:unique_categories handmade gardeing outdoor sports
+  - smembers amazon:unique_categories
+  - smembers amazon:unique_categories
+  - sadd twitter:hashtags help money trainings coding database
+  - smembers twitter:hashtags
+  - sadd twitter:hashtags help money trainings coding database stockmarkets klickanalytics
+  - smembers twitter:hashtags
   
 */
