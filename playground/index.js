a3af28afc60ff7b00d3ed2ea2321f39f772f9878
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 10:22:42 +0700

    Replace string value using SETRANGE

--- playground/index.js
+++ playground/index.js
@@ -1,19 +1,5 @@
 /*
-  Extract string value via GETRANGE
+  Replace string value using SETRANGE
   - pic
-
-  - set website "KlickAnalytics.com"
-  - get website
-  - getrange website 0 1
-  - getrange website 0 0
-  - getrange website 0 1
-  - getrange website 0 2
-  - getrange website 0 3
-  - getrange website 0 4
-  - getrange website 5 13
-  - getrange website 0 100
-  - getrange website -1 0
-  - getrange website 15 17
-  - getrange website -3 -1
 
 */
