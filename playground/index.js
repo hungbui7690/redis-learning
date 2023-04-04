b98a5d133a5ab1a426158961f9bf2dc00c03be96
hungbui7690 <hungbui7690@gmail.com>
2023-04-05 05:04:31 +0700

    Get random elements via SRANDMEMBER

--- playground/index.js
+++ playground/index.js
@@ -1,23 +1,12 @@
 /*
-  Checking a membership existence
+  Get random elements via SRANDMEMBER
   - pic
 
-  - smembers cars
-  - sismember cars ford
-  - sismember cars FORD
-  - sismember cars Ford
-  - sismember cars ford
-  - smembers subjects
-  - sadd subjects math ai english
-  - smembers subjects
-  - sismember subjects english
-  - sismember subjects ai
-  - smembers subjects
-  - sadd player:online alpha beta gamma
-  - smembers player:online
-  - sismember player:online beta
-  - sismember player:online amazing88
-  - smismember player:online alpha amazing88
-  - smismember subjects english technology
+  - sadd unique_lottery_num 1 20 30 40 50
+  - smembers unique_lottery_num
+  - srandmember unique_lottery_num 2
+  - srandmember unique_lottery_num 2
+  - srandmember unique_lottery_num 2
+  - srandmember unique_lottery_num 2
   
 */
