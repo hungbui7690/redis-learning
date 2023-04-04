2957c84e7593fd2770547655fba9d0321ff98cd6
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 08:19:57 +0700

    How to find data type of a key value - TYPE

--- playground/index.js
+++ playground/index.js
@@ -1,13 +1,18 @@
 /*
-  Deleting Keys Asynchronously via UNLINK
+  How to find data type of a key value - TYPE
   - pic
 
-  - set k1 v1
-  - set k2 v2
-  - del k1 k2
-  - set k1 v1
-  - set k2 v2
-  - unlink k1 k2
+  - set name "Joe"
+  - get name
+  - type name
 
+  - lpush k1 "value"
+    > lpush uses to push data to array
+  - type k1
+
+
+  - sadd k2 "value"
+    > sadd uses for "set"
+  - type k2
 
 */
