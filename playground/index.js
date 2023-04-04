f4df312ec53cec55a110eddb3888326fe7d6e960
hungbui7690 <hungbui7690@gmail.com>
2023-04-05 04:15:59 +0700

    Add, Get Members in Set

--- playground/index.js
+++ playground/index.js
@@ -1,5 +1,20 @@
 /*
-  
+  Add, Get Members in Set
+  - pics
+    - add set > use "sadd"
+    - to get the elements > "smembers"
 
+
+  - sadd cars toyota maserati ford
+  - sadd cars mazda
+  - smembers cars
+  - sadd names Rob Robby Rim John
+  - smembers names
+  - sadd numbers 1 2 3
+  - smembers numbers
+  - sadd numbers 6
+  - smembers numbers
+  - sadd cars ford
+  - smembers cars
   
 */
