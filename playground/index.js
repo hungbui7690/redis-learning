5b91bf7b513c6ecf66653c0a275fee5d5475d2ad
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 09:22:49 +0700

    Using APPEND as list of timeseries data

--- playground/index.js
+++ playground/index.js
@@ -1,20 +1,20 @@
 /*
-  Counting floating point numbers
+  Using APPEND as list of timeseries data
+  - add value to end of the string
   - pic
+  
 
-  - set num 1
-  - set num 1.5
-  - get num
-  - type num
-  - incrby num 1
-  - incrbyfloat num 1.2
-  - decrby num 1
-  - decrbyfloat num -1.2
-  - incrbyfloat num -1.2
-  - set app:fees:cc 1.0
-  - incrbyfloat app:fees:cc 0.2
-  - incrbyfloat app:fees:cc 2.0
-  - incrbyfloat app:fees:cc -1.5
-
-
+  - set title "hello"
+  - get title
+  - strlen title
+  - set website:stats:daily_visitors_log "100"
+  - get website:stats:daily_visitors_log
+  - append website:stats:daily_visitors_log "200"
+  - get website:stats:daily_visitors_log
+  - set website:stats:daily_visitors_log "100"
+  - get website:stats:daily_visitors_log
+  - set website:stats:daily_visitors_log " 200"
+  - get website:stats:daily_visitors_log
+  - append website:stats:daily_visitors_log " 3000"
+  - get website:stats:daily_visitors_log
 */
