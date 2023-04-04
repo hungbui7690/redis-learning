1cfc97487f4ef38df3f5f39f0d6b77d9b502e474
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 14:13:53 +0700

    Remove element via LREM

--- playground/index.js
+++ playground/index.js
@@ -1,30 +1,17 @@
 /*
-  Find matching elements via LPOS
+  Remove element via LREM
   - pic
 
-  - rpush mykey a b a a c d e f a
+  - flushdb
+  - rpush mykey "one" "one" "two" "three" "one"
   - lrange mykey 0 -1
-  - lpos mykey "a"
-  - lpos mykey "a" rank 0
-  - lpos mykey "a" rank 1
-  - lpos mykey "a" rank 2
-  - lpos mykey "a" rank 3
-  - lpos mykey "a" rank -1
-  - lpos mykey "a" rank -2
-  - lpos mykey "a" count
-  - lpos mykey "a" count 0
-  - lpos mykey "a" count 1
-  - lpos mykey "a" count 2
-  - lpos mykey "a" count 3
-  - lpos mykey "a" count 4
-  - lpos mykey "a" rank 1 count 0
-  - lpos mykey "a" rank -1 count 0
-  - lpos mykey "a" rank -1 count 1
-  - lpos mykey "a" rank -1 count 2
-  - lpos mykey "a" rank -1 count 2 maxlen 1
-  - lpos mykey "a" rank 1 count 2 maxlen 1
-  - lpos mykey "a" rank 1 count 2 maxlen 10
-  - lpos mykey "a" rank 1 count 2 maxlen 100
-  - lpos mykey "a" rank 1 count 0 maxlen 5000
+  - lrem mykey 0 "two"
+  - lrange mykey 0 -1
+  - lrem mykey 1 "one"
+  - lrange mykey 0 -1
+  - rpush mykey "five"
+  - lrange mykey 0 -1
+  - lrem mykey -1 "one"
+  - lrange mykey 0 -1
 
 */
