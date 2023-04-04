9f060cbe02ad84086d3ff95057a686a85d7ffa04
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 08:02:51 +0700

    Initial Setup

---
+++ # pictures/notes.js
@@ -0,0 +1,58 @@
+/*
+
+  
+  > docker-compose up --build -d
+  > 2 ways to work with redis: 
+    + docker-compose run rcli
+    + npx rbook
+      + http://localhost:3050 > rbook link
+
+
+
+
+  - Start
+    > sudo service redis-server start
+    > redis-cli
+
+    - ping
+    - ping "Hello"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
