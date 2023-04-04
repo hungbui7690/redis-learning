c982c8f70de7ef34cfb9a79bba7f0bcde88f60b3
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 11:19:54 +0700

    Using serialized JSON data

--- playground/index.js
+++ playground/index.js
@@ -1,15 +1,8 @@
 /*
-  String Encoding types
+  Using serialized JSON data
   - pic
 
-  - set mykey 1233456
-  - get mykey
-  - object encoding mykey
-  - set mykey "test string"
-  - object encoding mykey
-  - strlen mykey
-  - set mykey "This is a a long string defined for redis database structure"
-  - object encoding mykey
-
+  - set json '{"fname" : "John", "lname" : "Doe"}'
+  - get json
 
 */
