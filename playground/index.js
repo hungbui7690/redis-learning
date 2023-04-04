6daf2add8d7a33f90c71300156e35061adbc43cf
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 13:14:41 +0700

    Remove elements via LPOP and RPOP

--- playground/index.js
+++ playground/index.js
@@ -1,13 +1,19 @@
 /*
-  Insert an element via LINSERT
-  - pic
+  Remove elements via LPOP and RPOP
+  - pic 
 
-  - lrange dept 0 -1
-  - linsert dept befor "Admin" "Legal"
-  - lrange dept 0 -1
-  - linsert dept befor 0 "Legal"
-  - lrange dept 0 -1
-  - linsert dept befor "Sales" "Social"
-  - lrange dept 0 -1
+  - lpush num 1 2 3 4 5 6 7 8 9 10
+  - lrange num 0 -1
+  - del num
+  - lpush num 1 2 3 4 5 6 7 8 9 10
+  - lrange num 0 -1
+  - lpop num
+  - lrange num 0 -1
+  - rpop num
+  - rpop num
+  - lrange num 0 -1
+  - lpop num 2
+  - rpop num 1
+  - lrange num 0 -1
 
 */
