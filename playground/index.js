73046ce582d762718accaec6b314a87ae1f92a20
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 17:49:49 +0700

    Get all fields name via HKEYS

--- playground/index.js
+++ playground/index.js
@@ -1,10 +1,9 @@
 /*
-  Use HEXISTS to check if a field exist
+  Get all fields name via HKEYS
   - pic
 
-  - hexists user:101 fname
-  - hexists user:101 fname1
-  - hmget user:101 fname lname
-  - hmget user:101 fname lname1
+  - hgetall user:101
+  - hkeys user:101
+  - hkeys user:102
 
 */
