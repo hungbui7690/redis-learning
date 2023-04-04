1d61e92e47f1cf0a2559a1d495c33a4765da7e06
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 13:08:04 +0700

    Insert an element via LINSERT

--- playground/index.js
+++ playground/index.js
@@ -1,25 +1,13 @@
 /*
-  View latest stock prices via LINDEX
-  - get the element based on the index
+  Insert an element via LINSERT
   - pic
 
   - lrange dept 0 -1
-  - lindex dept 0
-  - lindex dept 1
-  - lindex dept 2
-  - lindex dept 3
-  - lindex dept -1
-  - lindex dept -2
-  - lindex dept 10
-  - lindex dept -10
-  - lpush MSFT:close_prices 10 10.20 11.00 20.00
-  - lrange MSFT:close_prices 0 -1
-  - lindex MSFT:close_prices - 1
-  - lindex MSFT:close_prices 0
-  - del MSFT:close_prices
-  - rpush MSFT:close_prices 10 10.20 15.00
-  - lrange MSFT:close_prices 0 -1
-  - lindex MSFT:close_prices -1
-  - lindex MSFT:close_prices -2
+  - linsert dept befor "Admin" "Legal"
+  - lrange dept 0 -1
+  - linsert dept befor 0 "Legal"
+  - lrange dept 0 -1
+  - linsert dept befor "Sales" "Social"
+  - lrange dept 0 -1
 
 */
