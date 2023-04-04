e689865cff62315b467c7934a1f5760d0d9006e9
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 17:10:58 +0700

    Find length of a hash via HLEN

--- playground/index.js
+++ playground/index.js
@@ -1,16 +1,8 @@
 /*
-  Get multiple fields values via HMGET
-  - in case we want to select specific fields, we don't can't use HGETALL
-    > HMGET <field> <field>
-
+  Find length of a hash via HLEN
   - pic
-  
-  - hgetall user
-  - rename user user:101
+
+  - hlen user:101
   - hgetall user:101
-  - hmget user:101 fname lname
-  - hmget user:101 fname lname lname1
-  - hmget user:101 age fname lname
-
-
+  
 */
