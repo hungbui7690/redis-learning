155b6e3c408c35cde03a5df9ec53cb309f1e3626
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 12:13:27 +0700

    Creating a list using LPUSH, RPUSH

--- playground/index.js
+++ playground/index.js
@@ -1,5 +1,15 @@
 /*
-  Introduction to lists
+  Creating a list using LPUSH, RPUSH
   - pic
+
+  - lpush dept "Sales"
+  - lpush dept "Admin" "HR"
+  - get deptlrange dept 0 -1
+  - lrange dept "Programming"
+  - lrange dept 0 -1
+  - rupsh dept "Marketing"
+  - lrange dept 0 -1
+  - lrange dept 1 -1
+  - lrange dept -3 -1
 
 */
