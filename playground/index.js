8baa562908c78c05349b5e63f5f33064c45aa6e8
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 17:59:36 +0700

    Counting via HINCRBY and HINCRBYFLOAT

--- playground/index.js
+++ playground/index.js
@@ -1,8 +1,16 @@
 /*
-  Get all fields values via HVALS
+  Counting via HINCRBY and HINCRBYFLOAT
   - pic
 
   - hgetall user:101
-  - hvals user:101
+  - hkeys user:101
+  - hset user:101 score:-10
+  - hgetall user:101
+  - hset user:101 commission 0.25
+  - hgetall user:101
+  - hincrbyfloat user:101 commission 1.2
+  - hgetall user:101
+  - hincrbyfloat user:101 commission -1.2
+  - hgetall user:101
 
 */
