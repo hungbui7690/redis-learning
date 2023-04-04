871bbe39a45622e1ea1db26d62031079b972a03b
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 08:33:40 +0700

    Redis Strings and their use cases

--- playground/index.js
+++ playground/index.js
@@ -1,6 +1,15 @@
 /*
-  Introduction to data types
+  Redis Strings and their use cases
   - pic
 
-  
+  - set otp:use_100 12345 ex 120
+  - get otp:use_100
+  - ttl otp:use_100
+  - set app:stats:daily_visitors 1000
+  - get app:stats:daily_visitors
+  - set app:config:title "KlickAnalytics"
+  - get app:config:title
+  - set app:users:types "Billable, Free"
+  - set app:config:usertimeout 10000
+
 */
