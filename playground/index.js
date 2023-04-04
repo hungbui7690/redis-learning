cac8419b0047ff79e2f150e13a616f281aed8af8
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 13:03:17 +0700

    View latest stock prices via LINDEX

--- playground/index.js
+++ playground/index.js
@@ -1,15 +1,25 @@
 /*
-  Creating a list using LPUSH, RPUSH
+  View latest stock prices via LINDEX
+  - get the element based on the index
   - pic
 
-  - lpush dept "Sales"
-  - lpush dept "Admin" "HR"
-  - get deptlrange dept 0 -1
-  - lrange dept "Programming"
   - lrange dept 0 -1
-  - rupsh dept "Marketing"
-  - lrange dept 0 -1
-  - lrange dept 1 -1
-  - lrange dept -3 -1
+  - lindex dept 0
+  - lindex dept 1
+  - lindex dept 2
+  - lindex dept 3
+  - lindex dept -1
+  - lindex dept -2
+  - lindex dept 10
+  - lindex dept -10
+  - lpush MSFT:close_prices 10 10.20 11.00 20.00
+  - lrange MSFT:close_prices 0 -1
+  - lindex MSFT:close_prices - 1
+  - lindex MSFT:close_prices 0
+  - del MSFT:close_prices
+  - rpush MSFT:close_prices 10 10.20 15.00
+  - lrange MSFT:close_prices 0 -1
+  - lindex MSFT:close_prices -1
+  - lindex MSFT:close_prices -2
 
 */
