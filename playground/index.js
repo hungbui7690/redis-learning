0444cbc0f69042b81708408f73e99f6a2f933228
hungbui7690 <hungbui7690@gmail.com>
2023-04-05 08:31:15 +0700

    Lexicographical order

--- playground/index.js
+++ playground/index.js
@@ -1,14 +1,13 @@
 /*
-  Atomic operations by ZINCRBY
+  Lexicographical order
   - pic
 
-  - zrange users:followers 0 -1 withscores
-  - zincrby users:followers 5 adam
-  - zrange users:followers 0 -1 withscores
-  - zincrby users:followers -5 scott
-  - zrange users:followers 0 -1 withscores
-  - zincrby users:followers 5 hans
-  - zrange users:followers 0 -1 withscores
-
-
+  - zadd num1:ss 1 a 2 b 3 c 3 d
+  - zrange num1:ss 0 -1 withscores
+  - zadd num2:ss 1 a 2 b 3 c 4 c
+  - zrange num2:ss 0 -1 withscores
+  - zadd num3:ss 1 a 2 b 3 c 4 ca
+  - zrange num3:ss 0 -1 withscores
+  - zrevrange num3:ss 0 -1 withscores
+  
 */
