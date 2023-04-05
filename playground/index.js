875890ac6a619e7db7b8a119505abcabc8a7b1e9
hungbui7690 <hungbui7690@gmail.com>
2023-04-05 09:37:35 +0700

    Patterned Subscription

--- playground/index.js
+++ playground/index.js
@@ -1,15 +1,24 @@
 /*
-  Setup a Publish_Subscribe communication
+  Patterned Subscription
   - pic
 
-  - subscribe ch1
-  - publish ch1 msg1
-  - publish ch1 msg2
-  - publish ch1 msg3
-  - publish ch1 msg4
-
-  - * Install telnet
-    - brew install telnet telnet
-    - o telnet 6379
+  On Publishing side
+  - publish news:tech "tech1"
+  - publish news:biz "biz1"
+  - publish news:tech "tech1"
+  - publish news:tech "tech1"
+  - publish news:politics "pol1"
+  - publish news:biz "biz3"
+  - publish topnews "top1"
+  - publish topnews "top1"
+  - 
+  On Subscriber side
+  - subscribe news:tech news:biz
+  - ctrl + c
+  - psubscribe news:*
+  - ctrl + c
+  - psubscribe news:* tech:*
+  - ctrl + c
+  - psubscribe news:* top*
 
 */
