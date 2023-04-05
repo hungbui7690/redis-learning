248560dc7cfa971334a2df9c87d29edef945e429
hungbui7690 <hungbui7690@gmail.com>
2023-04-05 07:21:22 +0700

    Sets Operations - Intersection

--- playground/index.js
+++ playground/index.js
@@ -1,13 +1,23 @@
 /*
-  Sets Operations - Union and store results with SUNIONSTORE
+  Sets Operations - Intersection
   - pic
 
-  - sunion num1 num2 num3
-  - sunionstore all_num num1 num2 num3
-  - smembers all_num
+  - sadd k1 a b c d
+  - sadd k2 c
+  - sadd k3 a c e
+  - smembers k1
+  - smembers k2
+  - smembers k3
+  - sinter k1 k2 k3
   - 
-  - sunion tasks:pending tasks:completed
-  - sunionstore tasks_2021_07 tasks:pending tasks:completed
-  - smembers tasks_2021_07
+  - sadd stocks:gainers AAPL MSFT IBM TSLA
+  - sadd stocks:vol_gainers AAPL MSFT QQQ
+  - sadd stocks:analyst AAPL C
+  - smembers stocks:gainers
+  - smembers stocks:vol_gainers
+  - smembers stocks:analyst
+  - sinter stocks:gainers stocks:vol_gainers stocks:analyst
+  - sinterstore stocks:best stocks:gainers stocks:vol_gainers stocks:analyst
+  - smembers stocks:best
   
 */
