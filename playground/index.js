d2f14739bb10cbb4be7e3880856340661715c5dd
hungbui7690 <hungbui7690@gmail.com>
2023-04-05 07:15:30 +0700

    Sets Operations - Union and store results with SUNIONSTORE

--- playground/index.js
+++ playground/index.js
@@ -1,19 +1,13 @@
 /*
-  Sets Operations - Union
+  Sets Operations - Union and store results with SUNIONSTORE
   - pic
 
-  - sadd num1 1 2 3 4 5
-  - sadd num2 2 4 6 8 10
-  - smembers num1
-  - smembers num2
-  - sunion num1 num2
-  - sadd num3 20 40 50
   - sunion num1 num2 num3
+  - sunionstore all_num num1 num2 num3
+  - smembers all_num
   - 
-  - sadd tickets:pending 1 2 3 4 5
-  - sadd tickets:completed 10 20 30 40 50
-  - smembers tickets:pending
-  - smembers tickets:completed
-  - sunion tickets:pending tickets:completed
+  - sunion tasks:pending tasks:completed
+  - sunionstore tasks_2021_07 tasks:pending tasks:completed
+  - smembers tasks_2021_07
   
 */
