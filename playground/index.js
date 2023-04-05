630b479452625ea2c7ff01c31417117c5361eb00
hungbui7690 <hungbui7690@gmail.com>
2023-04-05 07:48:18 +0700

    Using ZADD and ZRANGE

--- playground/index.js
+++ playground/index.js
@@ -1,5 +1,11 @@
 /*
-  Introduction to Sorted Sets
+  Using ZADD and ZRANGE
   - pic
+
+  - zadd users:followers adam 10
+  - zadd users:followers 10 adam 20 scott 30 amy
+  - zrange users:followers
+  - zrange users:followers 0 -1
+  - zrange users:followers 0 -1 withscores
   
 */
