acfd602db1d83b72fbd9758c247a109381a79d25
hungbui7690 <hungbui7690@gmail.com>
2023-04-05 09:26:16 +0700

    Setup a Publish_Subscribe communication

--- playground/index.js
+++ playground/index.js
@@ -1,5 +1,15 @@
 /*
-  Introduction to Publish_Subscribe Model
+  Setup a Publish_Subscribe communication
   - pic
+
+  - subscribe ch1
+  - publish ch1 msg1
+  - publish ch1 msg2
+  - publish ch1 msg3
+  - publish ch1 msg4
+
+  - * Install telnet
+    - brew install telnet telnet
+    - o telnet 6379
 
 */
