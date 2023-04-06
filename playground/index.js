56d6a665bd58b248c33b8d872cc9e07ed940734e
hungbui7690 <hungbui7690@gmail.com>
2023-04-06 07:38:43 +0700

    Insert country codes and stock prices via mass insertion

--- playground/index.js
+++ playground/index.js
@@ -1,8 +1,5 @@
 /*
-  Setup Docker Volume for Redis & Redis CLI
-  + docker-compose up --build -d
-  + docker-compose run rcli sh
-
-  *** Check notes.js for more info
+  Insert country codes and stock prices via mass insertion
+  - pic
 
 */
