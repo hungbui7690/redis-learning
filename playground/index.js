0eab43005f0a6d138c2f6340343c2fe9eb2c36c4
hungbui7690 <hungbui7690@gmail.com>
2023-04-06 08:40:26 +0700

    Dump and Restore Keys

--- playground/index.js
+++ playground/index.js
@@ -1,19 +1,15 @@
 /*
-  Inspect internal Redis objects via OBJECT
-  - pics
+  Dump and Restore Keys
+  - pic
 
-  - set hello 1
-  - set hello1 "Test"
-  - object refcount hello
-  - object refcount hello1
-  - object encoding hello
-  - object encoding hello1
-  - object idletime hello
-  - object idletime hello1
-  - lpush fruits "banana"
-  - object refcount fruits
-  - object encoding fruits
-  - object idletime fruits
-
+  - set name "Joe"
+  - get name
+  - dump name
+  - restore name 0 <paste code for key: name >
+  - get name
+  - del name
+  - restore name 0 <paste code for key: name >
+  - get name
+  - info
 
 */
