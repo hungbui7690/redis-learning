c6949f24188229674a43b8c3d261643f2cede28b
hungbui7690 <hungbui7690@gmail.com>
2023-04-06 07:56:06 +0700

    Introduction to Redis Protocol

--- playground/index.js
+++ playground/index.js
@@ -1,15 +1,6 @@
 /*
-  From multi primary keys to Redis Structure
-  - pics 
-
-  - hmset order:product:10248:11 unit_price 14 quantity 12 discount 0
-  - hmset order:product:10248:42 unit_price 9.8 quantity 10 discount 0
-  - hgetall order:product:10248:11
-  - hgetall order:product:10248:42
-  - keys order:product:10248:*
-  - keys order:product:*:11
-  - keys order:product:*:42
-  - keys order:product:*:*
+  Introduction to Redis Protocol
+  - pics
 
 
 */
