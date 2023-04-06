5dc670fe3eb570f8a9879bd06f08d5d5349d1656
hungbui7690 <hungbui7690@gmail.com>
2023-04-06 07:45:35 +0700

    From a RDBMS Table to Redis Data Structure

--- playground/index.js
+++ playground/index.js
@@ -1,5 +1,12 @@
 /*
-  Insert country codes and stock prices via mass insertion
+  From a RDBMS Table to Redis Data Structure
   - pic
+
+  - hmset employee:1 first_name John last_name Doe age 30 hire_date 2020-01-01 salary 15000
+  - hmset employee:2 first_name Betty last_name Kim age 30 hire_date 2020-10-01 salary 15000
+  - hgetall employee:1
+  - hgetall employee:2
+  - scan 0 match employee:*
+  - keys employee:*
 
 */
