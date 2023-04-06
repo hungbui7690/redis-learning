785004aed7250b6ff899fa573d5407978ab34a21
hungbui7690 <hungbui7690@gmail.com>
2023-04-06 09:09:04 +0700

    Using redis-cli to scan keys

--- playground/index.js
+++ playground/index.js
@@ -1,5 +1,19 @@
 /*
-  Checking command history
+  Using redis-cli to scan keys
   - pic
+
+  - set name1 1
+  - set name2 2
+  - set name3 e
+  - set name4 4
+  - debug populate 50
+  - redis-cli --scan
+  - redis-cli --scan --pattern '*'
+  - redis-cli --scan --pattern 'k*'
+  - redis-cli --scan --pattern '*key*'
+  - redis-cli --scan --pattern '*name*'
+  - redis-cli --scan --pattern '*' > keys.csv
+  - cat keys.csv
+  - redis-cli -h
 
 */
