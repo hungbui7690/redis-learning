c49115f84ea87bbc942f67be113bd8badf7cd888
hungbui7690 <hungbui7690@gmail.com>
2023-04-06 08:19:18 +0700

    Inspect internal Redis objects via OBJECT

--- # pictures/notes.js
+++ # pictures/notes.js
@@ -1,31 +1,56 @@
 /*
 
-  
-  > docker-compose up --build -d
-  > 2 ways to work with redis: 
+  > Step 1:
+    + docker-compose up --build -d
+
+  > Step 2a: This will run redis-cli
+    + docker-compose ps
     + docker-compose run rcli
-    + npx rbook
+      > open redis-cli
+
+  > Step 2b: This will run the container with Interactive Mode
+    + docker-compose run rcli sh
+
+  > Step 2c: 
+    + docker-compose exec -it redis sh
+
+/////////////////////////////////////////////
+
+  docker-compose up --build -d
+  docker-compose run -d rcli
+  docker-compose exec -it rcli sh
+
+/////////////////////////////////////////////
+
+  - npx rbook
       + http://localhost:3050 > rbook link
 
 
 
 
-  - Start
-    > sudo service redis-server start
-    > redis-cli
-
-    - ping
-    - ping "Hello"
 
 
 
 
-
-
-
-
-
-
+#### docker-compose.yaml
+version: '3'
+services:
+  redis:
+    image: 'redis:latest'
+  rcli:
+    image: 'redis:latest'
+    links:
+      - redis
+    volumes:
+      - redis-data:/data
+    command: redis-cli -h redis
+volumes:
+  redis-data:
+    driver: local
+    driver_opts:
+      type: 'none'
+      o: 'bind'
+      device: '/data'
 
 
 
--- playground/index.js
+++ playground/index.js
@@ -1,5 +1,19 @@
 /*
-  
+  Inspect internal Redis objects via OBJECT
+  - pics
 
-  
+  - set hello 1
+  - set hello1 "Test"
+  - object refcount hello
+  - object refcount hello1
+  - object encoding hello
+  - object encoding hello1
+  - object idletime hello
+  - object idletime hello1
+  - lpush fruits "banana"
+  - object refcount fruits
+  - object encoding fruits
+  - object idletime fruits
+
+
 */
