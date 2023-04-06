5010c1505aa1ae938fd98e7543de31cb70c7412f
hungbui7690 <hungbui7690@gmail.com>
2023-04-06 10:57:24 +0700

    Using URL, echo to list all keys

--- playground/index.js
+++ playground/index.js
@@ -1,13 +1,11 @@
 /*
-  Using bash to get all keys and values
+  Using URL, echo to list all keys
   - pic
-  - file: readkeys.sh
 
-
-  *** Redirect in Linux
-    > https://linuxhint.com/while_read_line_bash/
-    > https://tldp.org/LDP/intro-linux/html/sect_05_01.html
-    > http://mywiki.wooledge.org/BashFAQ/024
-
+  - URL=redis://localhost:6379
+  - echo "KEYS *" | \
+  - redis-cli -u $URL | \
+  - sed 's/^/GET /' | \
+  - redis-cli -u $URL
 
 */
