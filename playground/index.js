30f44e9ab05b690d2ca543d15b3ba5d09f825b59
hungbui7690 <hungbui7690@gmail.com>
2023-04-06 08:49:04 +0700

    Checking command history

--- playground/index.js
+++ playground/index.js
@@ -1,15 +1,5 @@
 /*
-  Dump and Restore Keys
+  Checking command history
   - pic
-
-  - set name "Joe"
-  - get name
-  - dump name
-  - restore name 0 <paste code for key: name >
-  - get name
-  - del name
-  - restore name 0 <paste code for key: name >
-  - get name
-  - info
 
 */
