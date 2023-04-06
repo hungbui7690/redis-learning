f3deef984e6c85177a7cda19557ba83cfa6eb3bd
hungbui7690 <hungbui7690@gmail.com>
2023-04-06 07:29:45 +0700

    Setup Docker Volume for Redis & Redis CLI

--- # pictures/notes.js
+++ # pictures/notes.js
@@ -1,21 +1,29 @@
 /*
 
-  
-  > docker-compose up --build -d
-  > 2 ways to work with redis: 
+  > Step 1:
+    + docker-compose up --build -d
+
+  > Step 2a: This will run redis-cli
+    + docker-compose ps
     + docker-compose run rcli
-    + npx rbook
+      > open redis-cli
+
+  > Step 2b: This will run the container with Interactive Mode
+    + docker-compose run rcli sh
+
+  > Step 2c: 
+    + docker-compose exec -it redis sh
+
+/////////////////////////////////////////////
+
+  docker-compose up --build -d
+  docker-compose run -d rcli
+  docker-compose exec -it rcli sh
+
+/////////////////////////////////////////////
+
+  - npx rbook
       + http://localhost:3050 > rbook link
-
-
-
-
-  - Start
-    > sudo service redis-server start
-    > redis-cli
-
-    - ping
-    - ping "Hello"
 
 
 
--- playground/index.js
+++ playground/index.js
@@ -1,5 +1,8 @@
 /*
-  
+  Setup Docker Volume for Redis & Redis CLI
+  + docker-compose up --build -d
+  + docker-compose run rcli sh
 
-  
+  *** Check notes.js for more info
+
 */
